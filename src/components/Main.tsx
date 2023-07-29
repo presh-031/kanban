da42a6ea1a41e286d0bdd91b60d288d0415a5e05
presh-031 <akingbuluge.dev@gmail.com>
2023-07-29 06:50:00 -0700

    chore: finished styling empty board

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,24 +1,29 @@
 import logoMobile from "../assets/assets/logo-mobile.svg";
 import arrowDown from "../assets/assets/icon-chevron-down.svg";
+import verticalEllipsis from "../assets/assets/icon-vertical-ellipsis.svg";
+import { GrAdd } from "react-icons/gr";
 
 const Main = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <header className="bg-white flex justify-between items-center">
+      <header className="bg-white px-[1.6rem] py-[2rem] flex justify-between items-center">
         <div className="flex items-center">
           <img src={logoMobile} alt="logo" />
-          <p>Platform Launch</p>
+          <p className="text-[1.8rem] mr-[.80rem] ml-[.80rem] text-[#000112] font-bold leading-normal">
+            Platform Launch
+          </p>
           <img src={arrowDown} alt="arrow-down" />
         </div>
-        <div>
+        <div className="flex items-center gap-[1.6rem]">
           <button
             role="button"
             disabled
-            className="py-[1.5rem] text-white px-[1.8rem] rounded-[2.4rem] bg-main-purple text-[1.5rem] font-bold leading-normal"
+            className="py-[1.5rem] disabled:opacity-25 disabled:cursor-not-allowed flex items-center justify-center px-[2.5rem] rounded-[2.4rem] bg-main-purple  font-bold leading-normal"
           >
-            + Add New Task
+            <GrAdd className="text-2xl" />
+            <span className="hidden text-[1.5rem]">Add New Task</span>
           </button>
-          <img src="" alt="" />
+          <img src={verticalEllipsis} alt="menu" />
         </div>
       </header>
 
