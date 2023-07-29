b9f56e7bcf5e895350a532c559d92b9afce983b2
presh-031 <akingbuluge.dev@gmail.com>
2023-07-29 08:44:17 -0700

    chore: onto using custom modal instead

--- src/components/TaskModal.tsx
+++ src/components/TaskModal.tsx
@@ -17,19 +17,42 @@ export default function TaskModal({ isOpen, setIsOpen }) {
     >
       <Dialog
         open={isOpen}
-        onClose={() => setIsOpen(false)}
+        onClose={() => setIsOpen(true)}
         className="relative z-50"
       >
         {/* The backdrop, rendered as a fixed sibling to the panel container */}
-        <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+        <div
+          className="fixed w-screen overflow-hidden inset-0 bg-black/30"
+          aria-hidden="true"
+        />
 
         {/* Full-screen container to center the panel */}
-        <div className="fixed inset-0 flex items-center justify-center p-4">
+        <div className="fixed w-screen  overflow-hidden h-screen inset-0 flex items-center justify-center p-4">
           {/* The actual dialog panel  */}
-          <Dialog.Panel className="mx-auto max-w-sm rounded bg-white">
-            <Dialog.Title>Complete your order</Dialog.Title>
+          <Dialog.Panel className="mx-auto max-w-sm w-[34.4rem] rounded bg-white">
+            <Dialog.Title className="font-bold text-[1.8rem] leading-normal text-black mb-[2.4rem]">
+              Research pricing points of various competitors and trial different
+              business models
+            </Dialog.Title>
+            <Dialog.Description>
+              We know what we're planning to build for version one. Now we need
+              to finalise the first pricing model we'll use. Keep iterating the
+              subtasks until we have a coherent proposition.
+            </Dialog.Description>
 
-            {/* ... */}
+            <div>
+              <p>Subtasks (2 of 3)</p>
+              <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+
+              <p>Current Status</p>
+            </div>
+
+            <button onClick={() => setIsOpen(false)}>Deactivate</button>
+            <button onClick={() => setIsOpen(false)}>Cancel</button>
           </Dialog.Panel>
         </div>
       </Dialog>
